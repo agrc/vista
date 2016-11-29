8484bf760c1c4a62ffa5b26aa69f35342ea30ccc
Scott Davis <stdavis@utah.gov>
2016-11-29 15:22:28 -0700

    remove political districts, zip and counties, and parcels

--- scripts/vista_pallet.py
+++ scripts/vista_pallet.py
@@ -20,32 +20,14 @@ class VistaPallet(Pallet):
         self.sgid = join(self.garage, 'SGID10.sde')
 
         self.political = join(self.staging, 'political.gdb')
-        self.boundaries = join(self.staging, 'boundaries.gdb')
-        self.cadastre = join(self.staging, 'cadastre.gdb')
         self.location = join(self.staging, 'location.gdb')
 
         self.copy_data = [self.political,
-                          self.boundaries,
-                          self.cadastre,
                           self.location]
 
     def build(self, config):
-        self.add_crates(['UtahHouseDistricts2012',
-                         'UtahSenateDistricts2012',
-                         'USCongressDistricts2012',
-                         'VistaBallotAreas',
+        self.add_crates(['VistaBallotAreas',
                          'VistaBallotAreas_Proposed'],
                         {'source_workspace': self.sgid,
                          'destination_workspace': self.political})
-        self.add_crates(['ZipCodes',
-                         'Counties'],
-                        {'source_workspace': self.sgid,
-                         'destination_workspace': self.boundaries})
-        self.add_crates(['Parcels_Beaver', 'Parcels_BoxElder', 'Parcels_Cache', 'Parcels_Carbon', 'Parcels_Daggett', 'Parcels_Davis', 'Parcels_Duchesne',
-                         'Parcels_Emery', 'Parcels_Garfield', 'Parcels_Grand', 'Parcels_Iron', 'Parcels_Juab', 'Parcels_Kane', 'Parcels_Millard',
-                         'Parcels_Morgan', 'Parcels_Piute', 'Parcels_Rich', 'Parcels_SaltLake', 'Parcels_SanJuan', 'Parcels_Sanpete', 'Parcels_Sevier',
-                         'Parcels_Summit', 'Parcels_Tooele', 'Parcels_Uintah', 'Parcels_Utah', 'Parcels_Wasatch', 'Parcels_Washington', 'Parcels_Wayne',
-                         'Parcels_Weber'],
-                        {'source_workspace': self.sgid,
-                         'destination_workspace': self.cadastre})
         self.add_crate(('AddressPoints', self.sgid, self.location))
